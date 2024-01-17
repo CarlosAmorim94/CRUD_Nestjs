4db7dbae8d4460846fc0116796c9296eb7b83c92
carlos <carlosa@xgrow.com>
2024-01-17 20:30:13 -0300

    Feat: Created userEntity

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { CreateUserDTO } from './dto/createUser.dto';
+import { UserEntity } from './user.entity';
 import { UserRepository } from './user.repository';
 
 @Controller('/users')
@@ -7,8 +9,14 @@ export class UserController {
   constructor(private userRepository: UserRepository) {}
   @Post()
   async criaUsuario(@Body() userData: CreateUserDTO) {
-    this.userRepository.save(userData);
-    return userData;
+    const userEntity = new UserEntity();
+    userEntity.name = userData.name;
+    userEntity.email = userData.email;
+    userEntity.password = userData.password;
+    userEntity.id = randomUUID();
+
+    this.userRepository.save(userEntity);
+    return { message: 'Usuário criado com sucesso' };
   }
 
   @Get()
